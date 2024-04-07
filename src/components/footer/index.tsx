101817f8a4510af005f30ab3a0352a8e23b9986c
unknown <lynda.alileche@gmail.com>
2024-04-07 21:45:54 +0200

    page footer et page Montion legale

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-4 absolute bottom-0 w-full"> 
+      <div className="container mx-auto text-center">
+        <p>Mention légale et CGU</p>
+        <p>CGU</p>
+        <p>Charte de protection des données personnelles</p>
+        <p>Tout droit réservé || Les Parfaits</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
