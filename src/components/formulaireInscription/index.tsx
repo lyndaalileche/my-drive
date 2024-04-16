219eaef2dbb41d9643c3632747c0e311302b3863
unknown <lynda.alileche@gmail.com>
2024-04-16 15:50:45 +0200

    page profil

--- src/components/formulaireInscription/index.tsx
+++ src/components/formulaireInscription/index.tsx
@@ -1,7 +1,5 @@
-'use client'
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, FormEvent, ChangeEvent } from "react";
 
-// Définition du type pour le state de l'utilisateur
 interface UserData {
   name: string;
   last_name: string;
@@ -9,34 +7,40 @@ interface UserData {
   password: string;
   confirmPassword: string;
 }
+
 const Inscription: React.FC = () => {
-    const initialUserData: UserData = {
-      name: '',
-      last_name: '',
-      email: '',
-      password: '',
-      confirmPassword: ''
-    };
+  const initialUserData: UserData = {
+    name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
 
   const [userData, setUserData] = useState<UserData>(initialUserData);
   const [passwordsMatch, setPasswordsMatch] = useState<boolean>(true);
+  const [passwordError, setPasswordError] = useState<string>("");
   const [acceptedTerms, setAcceptedTerms] = useState<boolean>(false);
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setUserData((prevUserData) => ({
-      ...prevUserData,
-      [name]: value
-    }));
-  };
-
   const handleTermsChange = () => {
     setAcceptedTerms(!acceptedTerms);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const validatePassword = (password: string) => {
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    return regex.test(password);
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    // Réinitialiser les messages d'erreur
+    setPasswordsMatch(true);
+    setPasswordError("");
 
     // Vérifier si les mots de passe correspondent
     if (userData.password !== userData.confirmPassword) {
@@ -44,33 +48,57 @@ const Inscription: React.FC = () => {
       return;
     }
 
-    // Vérifier si les conditions générales sont acceptées
-    if (!acceptedTerms) {
-      alert('Veuillez accepter les conditions générales d\'utilisation.');
+    // Vérifier la sécurité du mot de passe
+    if (!validatePassword(userData.password)) {
+      setPasswordError(
+        "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre."
+      );
+      if (!acceptedTerms) {
+        alert("Veuillez accepter les conditions générales d'utilisation.");
+        return;
+      }
+
       return;
     }
 
-    // Réinitialiser le message d'erreur des mots de passe
-    setPasswordsMatch(true);
+    // Si tout est valide, soumettre le formulaire
+    try {
+      const response = await fetch("http://localhost:3001/create-user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
 
-    // Ajouter ici la logique pour gérer la soumission du formulaire
-    console.log('Données utilisateur soumises :', userData);
-
-    // Réinitialiser les données du formulaire après la soumission
-    setUserData(initialUserData);
-    setAcceptedTerms(false); // Réinitialiser l'acceptation des conditions
+      if (!response.ok) {
+        throw new Error("La création de l’utilisateur a échoué");
+      }
+      const newUser: UserData = await response.json();
+      console.log("Utilisateur créé avec succès :", newUser);
+      alert("Inscription réussie!");
+    } catch (error) {
+      console.error(error);
+      alert("Erreur lors de la création de l’utilisateur.");
+    }
   };
 
   return (
-    <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
-      
-      <h2 className="text-xl font-semibold mb-4 text-center text-turquoise">Votre inscription</h2>
+    <main className="max-w-md mx-auto p-6 ">
+      <h1 className="text-xl font-semibold mb-4 text-center text-turquoise">
+        Votre inscription
+      </h1>
       <p className="text-gray-700 text-center mt-4">
-        Déjà inscrit ? <a href="/connexion" className="text-turquoise">Cliquez ici</a>
+        Déjà inscrit ?{" "}
+        <a href="/" className="text-turquoise">
+          Cliquez ici
+        </a>
       </p>
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
-          <label htmlFor="name" className="block mb-1">Prenom:</label>
+          <label htmlFor="name" className="block mb-1">
+            Prénom :
+          </label>
           <input
             type="text"
             id="name"
@@ -78,11 +106,13 @@ const Inscription: React.FC = () => {
             value={userData.name}
             onChange={handleChange}
             required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
+            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black shadow-gray-700 shadow-[5px_5px_5px_2px_rgba(0,0,0,0.5)]"
           />
         </div>
         <div>
-          <label htmlFor="last_name" className="block mb-1">Nom de famille :</label>
+          <label htmlFor="last_name" className="block mb-1">
+            Nom de famille :
+          </label>
           <input
             type="text"
             id="last_name"
@@ -90,11 +120,13 @@ const Inscription: React.FC = () => {
             value={userData.last_name}
             onChange={handleChange}
             required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
-            />
+            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black  shadow-gray-700 shadow-[5px_5px_5px_2px_rgba(0,0,0,0.5)]"
+          />
         </div>
         <div>
-          <label htmlFor="email" className="block mb-1">Email :</label>
+          <label htmlFor="email" className="block mb-1">
+            Email :
+          </label>
           <input
             type="email"
             id="email"
@@ -102,11 +134,13 @@ const Inscription: React.FC = () => {
             value={userData.email}
             onChange={handleChange}
             required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
-            />
+            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black  shadow-gray-700 shadow-[5px_5px_5px_2px_rgba(0,0,0,0.5)]"
+          />
         </div>
         <div>
-          <label htmlFor="password" className="block mb-1">Mot de passe :</label>
+          <label htmlFor="password" className="block mb-1">
+            Mot de passe :
+          </label>
           <input
             type="password"
             id="password"
@@ -114,11 +148,13 @@ const Inscription: React.FC = () => {
             value={userData.password}
             onChange={handleChange}
             required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
+            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black  shadow-gray-700 shadow-[5px_5px_5px_2px_rgba(0,0,0,0.5)]"
           />
         </div>
         <div>
-          <label htmlFor="confirmPassword" className="block mb-1">Confirmer le mot de passe :</label>
+          <label htmlFor="confirmPassword" className="block mb-1">
+            Confirmer le mot de passe :
+          </label>
           <input
             type="password"
             id="confirmPassword"
@@ -126,11 +162,14 @@ const Inscription: React.FC = () => {
             value={userData.confirmPassword}
             onChange={handleChange}
             required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
+            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black  shadow-gray-700 shadow-[5px_5px_5px_2px_rgba(0,0,0,0.5)]"
           />
           {!passwordsMatch && (
-            <p className="text-red-500">Les mots de passe ne correspondent pas.</p>
+            <p className="text-red-500">
+              Les mots de passe ne correspondent pas.
+            </p>
           )}
+          {passwordError && <p className="text-red-500">{passwordError}</p>}
         </div>
         <div className="flex items-center">
           <input
@@ -141,26 +180,30 @@ const Inscription: React.FC = () => {
             className="mr-2"
           />
           <label htmlFor="acceptTerms" className="text-sm text-gray-700">
-            J&apos;accepte les <a href="/conditions-generales" className="text-turquoise">conditions générales d&apos;utilisation</a>
+            J&apos;accepte les{" "}
+            <a href="/cgv" className="text-turquoise">
+              conditions générales d&apos;utilisation
+            </a>
           </label>
         </div>
         <div className="flex justify-between items-center">
-          <button
-            type="submit"
-            className="w-full bg-vert text-white px-4 py-1 rounded-2xl shadow-md"
-          >
-            Valider votre inscription
-          </button>
-          <div className="w-20"></div> 
+          {/* Inversion des boutons ici */}
           <button
             type="button"
             className="bg-white text-black px-4 py-1 rounded-2xl border border-black shadow-md"
           >
             Annuler
           </button>
+          <div className="w-20"></div>
+          <button
+            type="submit"
+            className="w-full bg-vert text-white px-4 py-1 rounded-2xl shadow-md"
+          >
+            Valider votre inscription
+          </button>
         </div>
-    </form>
-    </div>
+      </form>
+    </main>
   );
 };
 
---
+++ src/components/mainProfil/index.tsx
@@ -0,0 +1,134 @@
+"use client"
+import React, { useState } from "react";
+
+const MonProfil: React.FC = () => {
+  const [civility, setCivility] = useState<string>("");
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [confirmEmail, setConfirmEmail] = useState<string>("");
+
+  const handleCivilityChange = (selectedCivility: string) => {
+    setCivility(selectedCivility);
+  };
+
+  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleConfirmEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Civilité sélectionnée :", civility);
+    console.log("Prénom :", firstName);
+    console.log("Nom :", lastName);
+    console.log("Email :", email);
+    console.log("Confirmation Email :", confirmEmail);
+  };
+
+  return (
+    <main className="max-w-md mx-auto p-6">
+      <h1 className="text-xl font-semibold mb-4 text-center text-black">
+        Mon Profil
+      </h1>
+      <div className="border border-black p-6 rounded-lg">
+        <form onSubmit={handleSubmit} className="space-y-4">
+          {/* Section 1: Civilité */}
+          <div className="mb-4">
+            <label htmlFor="civility" className="block mb-1">
+              Civilité :
+            </label>
+            <div className="flex items-center">
+              <label htmlFor="madame" className="flex items-center mr-4">
+                <input
+                  type="radio"
+                  id="madame"
+                  name="civility"
+                  value="Madame"
+                  onChange={() => handleCivilityChange("Madame")}
+                />
+                Madame
+              </label>
+              <label htmlFor="monsieur" className="flex items-center">
+                <input
+                  type="radio"
+                  id="monsieur"
+                  name="civility"
+                  value="Monsieur"
+                  onChange={() => handleCivilityChange("Monsieur")}
+                />
+                Monsieur
+              </label>
+            </div>
+          </div>
+
+          {/* Section 2: Prénom */}
+          <div className="mb-4">
+            <label htmlFor="firstName" className="block mb-1">
+              Prénom :
+            </label>
+            <div className="w-full px-3 py-2 border rounded-3xl focus:outline-none shadow-gray-700 shadow-[5px_5px_5px_2px_rgba(0,0,0,0.5)] text-gray-500">
+              <p>Maxime</p>
+            </div>
+          </div>
+
+          {/* Section 3: Nom */}
+          <div className="mb-4">
+            <label htmlFor="lastName" className="block mb-1">
+              Nom :
+            </label>
+            <div className="w-full px-3 py-2 border rounded-3xl focus:outline-none shadow-gray-700 shadow-[5px_5px_5px_2px_rgba(0,0,0,0.5)] text-gray-500">
+              <p>Dupont</p>
+            </div>
+          </div>
+
+          {/* Section 4: Adresse Email et Confirmation */}
+          <div>
+            <label htmlFor="email" className="block mb-1">
+              Adresse Email :
+            </label>
+            <div className="w-full px-3 py-2 border rounded-3xl focus:outline-none shadow-gray-700 shadow-[5px_5px_5px_2px_rgba(0,0,0,0.5)] text-gray-500">
+              <p>test.dupont@gmail.com</p>
+            </div>
+          </div>
+
+          {/* Section 5: Mot de passe oublié */}
+          <div className="mb-4 ">
+            <a href="/Changer-mon-mot-de-passe" className="text-sm text-red-600">
+              Changer mon mot de passe
+            </a>
+          </div>
+
+          {/* Boutons Annuler et Valider (inversés) */}
+          <div className="flex flex-col md:flex-row md:justify-center md:space-x-4 mt-6">
+            <button
+              type="button"
+              className="bg-white text-turquoise px-11 py-1.5 rounded-lg border border-black shadow-md w-full md:w-auto mt-4 md:mt-0"
+            >
+              Annuler
+            </button>
+            <button
+              type="submit"
+              className="bg-vert text-white px-14 py-1.5 rounded-lg shadow-md w-full md:w-auto"
+            >
+              Valider
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default MonProfil;
