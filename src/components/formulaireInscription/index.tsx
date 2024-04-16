edce03b83e32c586a7f6753425535f438973bbcd
unknown <lynda.alileche@gmail.com>
2024-04-16 09:08:41 +0200

    sauvgarde

--- src/components/formulaireInscription/index.tsx
+++ src/components/formulaireInscription/index.tsx
@@ -1,7 +1,5 @@
-"use client";
 import React, { useState, FormEvent, ChangeEvent } from "react";
 
-// Définition du type pour le state de l'utilisateur
 interface UserData {
   name: string;
   last_name: string;
@@ -23,8 +21,9 @@ const Inscription: React.FC = () => {
   const [passwordsMatch, setPasswordsMatch] = useState<boolean>(true);
   const [passwordError, setPasswordError] = useState<string>("");
   const [acceptedTerms, setAcceptedTerms] = useState<boolean>(false);
+
   const handleTermsChange = () => {
-    setAcceptedTerms(!acceptedTerms);
+    setAcceptedTerms(!acceptedTerms); // Inverse l'état du bouton lorsque cliqué
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,18 +38,15 @@ const Inscription: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    // Réinitialiser les messages d'erreur
+    
     setPasswordsMatch(true);
     setPasswordError("");
 
-    // Vérifier si les mots de passe correspondent
     if (userData.password !== userData.confirmPassword) {
       setPasswordsMatch(false);
       return;
     }
 
-    // Vérifier la sécurité du mot de passe
     if (!validatePassword(userData.password)) {
       setPasswordError(
         "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre."
@@ -59,11 +55,9 @@ const Inscription: React.FC = () => {
         alert("Veuillez accepter les conditions générales d'utilisation.");
         return;
       }
-
       return;
     }
 
-    // Si tout est valide, soumettre le formulaire
     try {
       const response = await fetch("http://localhost:3001/create-user", {
         method: "POST",
@@ -86,7 +80,7 @@ const Inscription: React.FC = () => {
   };
 
   return (
-    <main className="max-w-md mx-auto p-6 ">
+    <main className="max-w-md mx-auto p-6">
       <h1 className="text-xl font-semibold mb-4 text-center text-turquoise">
         Votre inscription
       </h1>
@@ -96,83 +90,8 @@ const Inscription: React.FC = () => {
           Cliquez ici
         </a>
       </p>
-      <form  onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label htmlFor="name" className="block mb-1">
-            Prenom:
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={userData.name}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
-          />
-        </div>
-        <div>
-          <label htmlFor="last_name" className="block mb-1">
-            Nom de famille :
-          </label>
-          <input
-            type="text"
-            id="last_name"
-            name="last_name"
-            value={userData.last_name}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
-          />
-        </div>
-        <div>
-          <label htmlFor="email" className="block mb-1">
-            Email :
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={userData.email}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
-          />
-        </div>
-        <div>
-          <label htmlFor="password" className="block mb-1">
-            Mot de passe :
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={userData.password}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
-          />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword" className="block mb-1">
-            Confirmer le mot de passe :
-          </label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={userData.confirmPassword}
-            onChange={handleChange}
-            required
-            className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:border-vert bordure to-black"
-          />
-          {!passwordsMatch && (
-            <p className="text-red-500">
-              Les mots de passe ne correspondent pas.
-            </p>
-          )}
-          {passwordError && <p className="text-red-500">{passwordError}</p>}
-        </div>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        {/* Autres champs de formulaire ici */}
         <div className="flex items-center">
           <input
             type="checkbox"
@@ -182,7 +101,10 @@ const Inscription: React.FC = () => {
             className="mr-2"
           />
           <label htmlFor="acceptTerms" className="text-sm text-gray-700">
-            J&apos;accepte les{" "}
+            {acceptedTerms
+              ? "Utiliser client"
+              : "Ne pas utiliser client"}{" "}
+            {/* Texte inversé du bouton */}
             <a href="/conditions-generales" className="text-turquoise">
               conditions générales d&apos;utilisation
             </a>
