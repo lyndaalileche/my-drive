9fa91e022c3af1685c726a387eb1857fc3a268ab
VMWebPro <vmwebpro@gmail.com>
2024-04-16 09:30:57 +0200

    change sur src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,14 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import MainAcceuil from "@/components/mainAccueil";
 
-
-
 export default function Home() {
-   
-  return (
-    <main className="font-LucioleRegular">
-      <Navbar/>
-      <MainAcceuil/>
-      <Footer/>
-    </main>
-  );
+    return (
+        <div>
+            <Navbar />
+            <main className="font-LucioleRegular">
+                <MainAcceuil />
+            </main>
+            <Footer />
+        </div>
+    );
 }
